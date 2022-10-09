fc6a1dc1714850661bfa68a62268e2e2186bee6d
Jay Alvin C. Vallescas <202080209@psu.palawan.edu.ph>
2022-10-09 15:24:16 +0800

    Modify private message class  back-color

--- notifications-page-main/script.js
+++ notifications-page-main/script.js
@@ -3,12 +3,6 @@ let readComment = document.getElementById('num');
 let stat = document.getElementsByClassName("status");
 let box_content = document.getElementsByClassName("box-content-box");
 
-
-function userClick() {
-    userComment.style.background = "none";
-    userComment.style.border = '1px solid var(--Grayish_blue)';
-}
-
 read = () => {
     readComment.innerText = 0;
     for(let i = 0; i<=stat.length; i++){
@@ -16,3 +10,7 @@ read = () => {
         box_content[i].style.background = "none";
     }
 }
+
+
+
+
